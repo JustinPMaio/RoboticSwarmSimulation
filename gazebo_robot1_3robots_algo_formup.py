2457cc9bd4caba3fa5de3c807416ba4599761340
JustinPMaio <100385502+JustinPMaio@users.noreply.github.com>
2022-02-24 17:27:13 -0500

    Add files via upload

---
+++ gazebo_robot1_3robots_algo_formup.py
@@ -0,0 +1,351 @@
+#!/usr/bin/env python
+import math
+from math import atan2, degrees
+import rospy
+import time
+from geometry_msgs.msg import Twist
+import numpy as np
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import LaserScan
+import sys
+import signal
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
+from std_msgs.msg import String
+from robot import Robot, Spot
+object_ahead = bool(False)
+robot_ahead = bool(False)
+roll = pitch = yaw = 0.0
+orders = " "
+Ready = bool(False)
+assigned = bool(False)
+right = bool(False)
+TargetX = float(0)
+TargetY = float(0)
+
+R1 = Robot(0, 0, 0, 0)
+R2 = Robot(0, 0, 0, 0)
+R3 = Robot(0, 0, 0, 0)
+S1 = Spot(0, 0, 1)
+S2 = Spot(0, 0, 2)
+S3 = Spot(0, 0, 3)
+S1.number = 1
+S2.number = 2
+S3.number = 3
+
+
+def update_orders(msg):
+	global orders
+	orders = msg.data
+	print(orders)
+
+
+def get_rotation_pos(msg):
+	global roll, pitch, yaw
+	orientation_q = msg.pose.pose.orientation
+	orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
+	(roll, pitch, yaw) = euler_from_quaternion (orientation_list)
+	R1.heading = yaw
+	R1.x = float(msg.pose.pose.position.x)
+	R1.y = float(msg.pose.pose.position.y)
+
+
+def check_laser(msg):
+	global object_ahead
+	object_ahead = False
+	object_ahead_distance = min(msg.ranges[0], msg.ranges[10], msg.ranges[20], msg.ranges[350], msg.ranges[340])
+	if object_ahead_distance < 0.8:
+		object_ahead = True
+		print("object ahead")
+
+
+def check_robot2(msg):
+	global robot_ahead
+	robot_ahead = False
+	R2.x = float(msg.pose.pose.position.x)
+	R2.y = float(msg.pose.pose.position.y)
+	distance = math.sqrt((R2.x - R1.x)**2 + (R2.y - R1.y)**2)
+	if distance < 1:  # only do any of this if robot2 is close
+		Xdistance = R2.x - R1.x
+		Ydistance = R2.y - R1.y
+		if (-0.25*np.pi) < R1.heading <= (0.75*np.pi):  # check northwest
+			if Xdistance >= 0 and Ydistance >= 0:
+				robot_ahead = True
+		if (0.25*np.pi) < R1.heading <= (1.25*np.pi):  # check southwest
+			if Xdistance <= 0 and Ydistance >= 0:
+				robot_ahead = True
+		if (-1.25*np.pi) < R1.heading <= (-0.25*np.pi):  # check southeast
+			if Xdistance <= 0 and Ydistance <= 0:
+				robot_ahead = True
+		if (-0.75*np.pi) < R1.heading <= (0.25*np.pi):  # check northeast
+			if Xdistance >= 0 and Ydistance <= 0:
+				robot_ahead = True
+
+
+def check_robot3(msg):
+	global robot_ahead
+	R3.x = float(msg.pose.pose.position.x)
+	R3.y = float(msg.pose.pose.position.y)
+	distance = math.sqrt((R3.x - R1.x)**2 + (R3.y - R1.y)**2)
+	if distance < 1:  # only do any of this if robot3 is close
+		Xdistance = R3.x - R1.x
+		Ydistance = R3.y - R1.y
+		if (-0.25*np.pi) < R1.heading <= (0.75*np.pi):  # check northwest
+			if Xdistance >= 0 and Ydistance >= 0:
+				robot_ahead = True
+		if (0.25*np.pi) < R1.heading <= (1.25*np.pi):  # check southwest
+			if Xdistance <= 0 and Ydistance >= 0:
+				robot_ahead = True
+		if (-1.25*np.pi) < R1.heading <= (-0.25*np.pi):  # check southeast
+			if Xdistance <= 0 and Ydistance <= 0:
+				robot_ahead = True
+		if (-0.75*np.pi) < R1.heading <= (0.25*np.pi):  # check northeast
+			if Xdistance >= 0 and Ydistance <= 0:
+				robot_ahead = True
+
+
+def go(msg):
+	global object_ahead
+	global orders
+	global assigned
+	global TargetX
+	global TargetY
+	global Ready
+	global right
+	all_robots = [R1, R2, R3]
+	all_spots = [S1, S2, S3]
+	if orders == "1":
+		assigned = False
+		right = False
+		for item in all_robots:
+			item.spot = 0
+		for item in all_spots:
+			item.occupied = False
+		if object_ahead:
+			move = Twist()
+			move.linear.x = 0.0
+			move.angular.z = 1.0
+			move.linear.y = 0.0
+			pub = rospy.Publisher("/robot1/cmd_vel", Twist, queue_size=1)
+			pub.publish(move)
+		if robot_ahead:
+			move = Twist()
+			move.linear.x = 0.0
+			move.angular.z = 0.5
+			move.linear.y = 0.0
+			pub = rospy.Publisher("/robot1/cmd_vel", Twist, queue_size=1)
+			pub.publish(move)
+		if not object_ahead and not robot_ahead:
+			move = Twist()
+			move.linear.x = 0.5
+			move.angular.z = 0.0
+			move.linear.y = 0.0
+			pub = rospy.Publisher("/robot1/cmd_vel", Twist, queue_size=1)
+			pub.publish(move)
+	if orders == "0":
+		assigned = False
+		for item in all_robots:
+			item.spot = 0
+		for item in all_spots:
+			item.occupied = False
+		assigned = False
+		right = False
+		move = Twist()
+		move.linear.x = 0.0
+		move.angular.z = 0.0
+		move.linear.y = 0.0
+		pub = rospy.Publisher("/robot1/cmd_vel", Twist, queue_size=2)
+		pub.publish(move)
+	if orders == "2":
+		S1.x = 0
+		S1.y = 0
+		S2.x = 1
+		S2.y = 0
+		S3.x = 2
+		S3.y = 0
+	if orders == "3":
+		S1.x = 2
+		S1.y = 2
+		S2.x = 1
+		S2.y = 1
+		S3.x = 2
+		S3.y = 0
+	if orders == "5":
+		S1.x = 2
+		S1.y = 1.5
+		S2.x = 1.25
+		S2.y = 1.2
+		S3.x = 1.25
+		S3.y = 1.7
+	if orders == "2" or "3" or "4" or "5":
+		Ready = False
+		center_x = float((S1.x + S2.x + S3.x)/3)
+		center_y = float((S1.y + S2.y + S3.y)/3)
+		if not assigned:
+			print("robots are not assigned yet")
+			move = Twist()
+			move.linear.x = 0.0
+			move.angular.z = 0.0
+			move.linear.y = 0.0
+			pub = rospy.Publisher("/robot1/cmd_vel", Twist, queue_size=2)
+			pub.publish(move)
+			R1Dist = math.sqrt(((center_x - R1.x)**2)+((center_y - R1.y)**2))  # distance r1's position to "formation center"
+			R2Dist = math.sqrt(((center_x - R2.x)**2)+((center_y - R2.y)**2))
+			R3Dist = math.sqrt(((center_x - R3.x)**2)+((center_y - R3.y)**2))
+			robot_distances = [0.0, R1Dist, R2Dist, R3Dist]
+			available_spots = [S1, S2, S3]
+			decision_message = " "
+			while not assigned:
+				if max(robot_distances) == R1Dist:
+					robot_distances.remove(R1Dist)
+					for item in available_spots:
+						if item.occupied:
+							available_spots.remove(item)
+					check_distance = 100
+					for item in available_spots:
+						potential_distance = math.sqrt((item.x - R1.x)**2 + (item.y - R1.y)**2)
+						if potential_distance < check_distance:
+							check_distance = potential_distance
+							R1.spot = item.number 
+					for item in available_spots:
+						if R1.spot == item.number:
+							item.occupied = True
+					decision_message += " R1spot" + str(R1.spot) 
+				if len(robot_distances) == 1:
+					assigned = True
+					break
+				if max(robot_distances) == R2Dist:
+					robot_distances.remove(R2Dist)
+					for item in available_spots:
+						if item.occupied:
+							available_spots.remove(item)
+					check_distance = 100
+					for item in available_spots:
+						potential_distance = math.sqrt((item.x - R2.x)**2 + (item.y - R2.y)**2)
+						if potential_distance < check_distance:
+							check_distance = potential_distance
+							R2.spot = item.number 
+					for item in available_spots:
+						if R2.spot == item.number:
+							item.occupied = True
+					decision_message += " R2spot" + str(R2.spot) 
+				if len(robot_distances) == 1:
+					assigned = True
+					break
+				if max(robot_distances) == R3Dist:
+					robot_distances.remove(R3Dist)
+					for item in available_spots:
+						if item.occupied:
+							available_spots.remove(item)
+					check_distance = 100
+					for item in available_spots:
+						potential_distance = math.sqrt((item.x - R3.x)**2 + (item.y - R3.y)**2)
+						if potential_distance < check_distance:
+							check_distance = potential_distance
+							R3.spot = item.number 
+					for item in available_spots:
+						if R3.spot == item.number:
+							item.occupied = True
+				decision_message += " R3spot" + str(R3.spot) 
+				if len(robot_distances) == 1:
+					assigned = True
+					break
+			print(decision_message)
+			pub2 = rospy.Publisher('/robot1/chatter', String, queue_size=1)
+			rate = rospy.Rate(10)
+			pub2.publish(decision_message)
+		for item in all_spots:
+			if R1.spot == item.number:  # the robot needs to point to its assigned position.
+				angle = atan2(R1.y - item.y, R1.x - item.x) - np.pi
+				TargetX = item.x
+				TargetY = item.y
+		if angle < -3.1415:
+			angle += (2*np.pi)  # important line of code to make sure the angle value stays in the correct range of values
+		if (angle-0.3) < R1.heading < (angle + 0.3):
+			Ready = True
+			move = Twist()
+			move.linear.x = 0.0
+			move.angular.z = 0.0
+			move.linear.y = 0.0
+			pub = rospy.Publisher("/robot1/cmd_vel", Twist, queue_size=2)
+			pub.publish(move)
+		if not Ready:
+			target_distance = math.sqrt(((R1.y-TargetY)**2)+((R1.x - TargetX)**2))
+			if target_distance > 0.05:
+				if 0 <= R1.heading < (np.pi/2):  # get the rover to turn right in some cases, depending on its quadrant
+					if (np.pi/-2) <= angle <= 0:
+						right = True
+				if (np.pi/-2) <= R1.heading < 0:
+					if (np.pi*-1) <= angle <= (np.pi/-2):
+						right = True
+				if (np.pi/-1) <= R1.heading < (np.pi/-2):
+					if (np.pi/2) <= angle <= np.pi:
+						right = True
+				if (np.pi/2) <= R1.heading < np.pi:
+					if 0 <= angle <= (np.pi/2):
+						right = True
+				move = Twist()
+				move.linear.x = 0.0
+				move.angular.z = 1.0
+				move.linear.y = 0.0
+				if right:
+					move.angular.z = -1.0
+				pub = rospy.Publisher("/robot1/cmd_vel", Twist, queue_size=2)
+				pub.publish(move)
+				print("must face target")
+			if target_distance <= 0.05:
+				move = Twist()
+				move.linear.x = 0.0
+				move.angular.z = 0.0
+				move.linear.y = 0.0
+				pub = rospy.Publisher("/robot1/cmd_vel", Twist, queue_size=2)
+				pub.publish(move)
+				print("reached target")
+		if Ready:
+			print("drive ahead")
+			right = False
+			target_distance = math.sqrt(((R1.y-TargetY)**2)+((R1.x - TargetX)**2))
+			if target_distance > 0.05:
+				move = Twist()
+				move.linear.x = 0.5
+				if robot_ahead:
+					move.linear.x = 0.1
+				move.angular.z = 0.0
+				move.linear.y = 0.0
+				pub = rospy.Publisher("/robot1/cmd_vel", Twist, queue_size=2)
+				pub.publish(move)
+				print(target_distance)
+			if target_distance <= 0.05:
+				move = Twist()
+				move.linear.x = 0.0
+				move.angular.z = 0.0
+				move.linear.y = 0.0
+				pub = rospy.Publisher("/robot1/cmd_vel", Twist, queue_size=2)
+				pub.publish(move)
+				print("reached target")
+
+
+def signal_handler(signal, frame):
+	move = Twist()
+	move.linear.x = 0.0
+	move.angular.z = 0.0
+	move.linear.y = 0.0
+	pub = rospy.Publisher("/robot1/cmd_vel", Twist, queue_size=2)
+	pub.publish(move)
+	print("exiting program")
+	sys.exit(0)
+
+
+def listener():
+	rospy.init_node('listener')
+	rospy.Subscriber("/chatter", String, update_orders)
+	rospy.Subscriber("/robot1/scan", LaserScan, check_laser)
+	rospy.Subscriber("/robot1/odom", Odometry, get_rotation_pos)
+	rospy.Subscriber("/robot2/odom", Odometry, check_robot2)
+	rospy.Subscriber("/robot3/odom", Odometry, check_robot3)
+	rospy.Subscriber("/robot1/odom", Odometry, get_rotation_pos)
+	rospy.Subscriber("/robot1/odom", Odometry, go)
+	rospy.spin()
+
+
+if __name__ == '__main__':
+	listener()
---
+++ gazebo_robot2_3robots_algo_formup.py
@@ -0,0 +1,445 @@
+#!/usr/bin/env python
+import math
+from math import atan2, degrees
+import rospy
+import time
+from geometry_msgs.msg import Twist
+import numpy as np
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import LaserScan
+import sys
+import signal
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
+from std_msgs.msg import String
+MyX = float(0)
+MyY = float(0)
+R1X = float(0)
+R1Y = float(0)
+R3X = float(0)
+R3Y = float(0)
+object_ahead = bool(False)
+robot_ahead = bool(False)	
+roll = pitch = yaw = 0.0
+MyHeading = 0
+distance = 10
+orders = " "
+Ready = bool(False)
+position1 = bool(False)
+position2 = bool(False)
+position3 = bool(False)
+assigned = bool(False)
+TargetX = float(0)
+TargetY = float(0)
+right = False
+
+
+def update_orders(msg):
+	global orders
+	orders = msg.data
+	print(orders)
+
+
+def get_rotation_pos(msg):
+	global roll, pitch, yaw
+	global MyX
+	global MyY
+	global MyHeading
+	orientation_q = msg.pose.pose.orientation
+	orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
+	(roll, pitch, yaw) = euler_from_quaternion (orientation_list)
+	MyHeading = yaw
+	MyX = float(msg.pose.pose.position.x)
+	MyY = float(msg.pose.pose.position.y)
+
+
+def check_laser(msg):
+	global object_ahead
+	object_ahead = False
+	object_ahead_distance = min(msg.ranges[0], msg.ranges[10], msg.ranges[20], msg.ranges[350], msg.ranges[340])
+	if object_ahead_distance < 0.8:
+		object_ahead = True
+
+
+def check_robot1(msg):
+	global robot_ahead
+	global R1X
+	global R1Y
+	robot_ahead = False
+	R1X = float(msg.pose.pose.position.x)
+	R1Y = float(msg.pose.pose.position.y)
+	distance = math.sqrt((R1X-MyX)**2 + (R1Y-MyY)**2)
+	if distance < 1:  # only do any of this if robot2 is close
+		Xdistance = R1X-MyX
+		Ydistance = R1Y-MyY
+		if (-0.25*np.pi) < MyHeading <= (0.75*np.pi):  # check northwest
+			if Xdistance >= 0 and Ydistance >= 0:
+				robot_ahead = True
+		if (0.25*np.pi) < MyHeading <= (1.25*np.pi):  # check southwest
+			if Xdistance <= 0 and Ydistance >= 0:
+				robot_ahead = True
+		if (-1.25*np.pi) < MyHeading <= (-0.25*np.pi):  # check southeast
+			if Xdistance <= 0 and Ydistance <= 0:
+				robot_ahead = True
+		if (-0.75*np.pi) < MyHeading <= (0.25*np.pi):  # check northeast
+			if Xdistance >= 0 and Ydistance <= 0:
+				robot_ahead = True
+
+
+def check_robot3(msg):
+	global robot_ahead
+	global R3X
+	global R3Y
+	R3X = float(msg.pose.pose.position.x)
+	R3Y = float(msg.pose.pose.position.y)
+	distance = math.sqrt((R3X-MyX)**2 + (R3Y-MyY)**2)
+	if distance < 1:  # only do any of this if robot3 is close
+		Xdistance = R3X-MyX
+		Ydistance = R3Y-MyY
+		if (-0.25*np.pi) < MyHeading <= (0.75*np.pi):  # check northwest
+			if Xdistance >= 0 and Ydistance >= 0:
+				robot_ahead = True
+		if (0.25*np.pi) < MyHeading <= (1.25*np.pi):  # check southwest
+			if Xdistance <= 0 and Ydistance >= 0:
+				robot_ahead = True
+		if (-1.25*np.pi) < MyHeading <= (-0.25*np.pi):  # check southeast
+			if Xdistance <= 0 and Ydistance <= 0:
+				robot_ahead = True
+		if (-0.75*np.pi) < MyHeading <= (0.25*np.pi):  # check northeast
+			if Xdistance >= 0 and Ydistance <= 0:
+				robot_ahead = True
+
+
+def check_assignment(msg):
+	global assigned
+	global position1
+	global position2
+	global position3
+	global TargetX
+	global TargetY
+	assignment = msg.data
+	print(assignment)
+	if "R2spot1" in assignment:
+		position1 = True
+		assigned = True
+		print("I am first")
+		if orders == "2":
+			TargetX = 0
+			TargetY = 0
+		if orders == "3":
+			TargetX = 2
+			TargetY = 2
+		if orders == "5":
+			TargetX = 2
+			TargetY = 1.5
+	if "R2spot2" in assignment:
+		position2 = True
+		assigned = True
+		print("I am second")
+		if orders == "2":
+			TargetX = 1
+			TargetY = 0
+		if orders == "3":
+			TargetX = 1
+			TargetY = 1
+		if orders == "5":
+			TargetX = 1.25
+			TargetY = 1.2
+	if "R2spot3" in assignment:
+		position3 = True	
+		assigned = True	 
+		print("I am third")
+		if orders == "2":
+			TargetX = 2
+			TargetY = 0
+		if orders == "3":
+			TargetX = 2
+			TargetY = 0
+		if orders == "5":
+			TargetX = 1.25
+			TargetY = 1.7
+
+
+def go(msg):
+	global object_ahead
+	global orders
+	global Ready
+	global assigned
+	global position1
+	global position2
+	global position3
+	global right
+	if orders == "1":
+		assigned = False
+		position1 = False
+		position2 = False
+		position3 = False
+		if object_ahead or robot_ahead:
+			move = Twist()
+			move.linear.x = 0.0
+			move.angular.z = 1.0
+			move.linear.y = 0.0
+			pub = rospy.Publisher("/robot2/cmd_vel", Twist, queue_size=2)
+			pub.publish(move)
+		if not object_ahead and not robot_ahead:
+			move = Twist()
+			move.linear.x = 0.5
+			move.angular.z = 0.0
+			move.linear.y = 0.0
+			pub = rospy.Publisher("/robot2/cmd_vel", Twist, queue_size=2)
+			pub.publish(move)
+	if orders == "0":
+		position1 = False
+		position2 = False
+		position3 = False
+		assigned = False
+		move = Twist()
+		move.linear.x = 0.0
+		move.angular.z = 0.0
+		move.linear.y = 0.0
+		pub = rospy.Publisher("/robot2/cmd_vel", Twist, queue_size=2)
+		pub.publish(move)
+	if orders == "2":
+		Ready = False
+		if assigned:
+			if position1:
+				angle = atan2(MyY, MyX) - np.pi
+			if position2:
+				angle = atan2(MyY, (MyX-1)) - np.pi
+			if position3:
+				angle = atan2(MyY, (MyX-2)) - np.pi
+			if angle < -3.1415:
+				angle += (2*np.pi)
+			if (angle-0.3) < MyHeading < (angle + 0.3):
+				Ready = True
+				move = Twist()
+				move.linear.x = 0.0
+				move.angular.z = 0.0
+				move.linear.y = 0.0
+				pub = rospy.Publisher("/robot2/cmd_vel", Twist, queue_size=2)
+				pub.publish(move)
+			if not Ready:
+				target_distance = math.sqrt(((MyY-TargetY)**2)+((MyX-TargetX)**2))
+				if target_distance > 0.1:
+					if 0 <= MyHeading < (np.pi/2):  # get the rover to turn right in some cases, depending on its quadrant
+						if (np.pi/-2) <= angle <= 0:
+							right = True
+					if (np.pi/-2) <= MyHeading < 0:
+						if (np.pi*-1) <= angle <= (np.pi/-2):
+							right = True
+					if (np.pi/-1) <= MyHeading < (np.pi/-2):
+						if (np.pi/2) <= angle <= np.pi:
+							right = True
+					if (np.pi/2) <= MyHeading < np.pi:
+						if 0 <= angle <= (np.pi/2):
+							right = True
+					move = Twist()
+					move.linear.x = 0.0
+					move.angular.z = 1.0
+					move.linear.y = 0.0
+					if right:
+						move.angular.z = -1.0
+					pub = rospy.Publisher("/robot2/cmd_vel", Twist, queue_size=2)
+					pub.publish(move)
+					print("must face target")
+				if target_distance <= 0.1:
+					move = Twist()
+					move.linear.x = 0.0
+					move.angular.z = 0.0
+					move.linear.y = 0.0
+					pub = rospy.Publisher("/robot2/cmd_vel", Twist, queue_size=2)
+					pub.publish(move)
+					print("reached target")
+			if Ready:
+				right = False
+				target_distance = math.sqrt(((MyY-TargetY)**2)+((MyX-TargetX)**2))
+				if target_distance > 0.1:
+					move = Twist()
+					move.linear.x = 0.5
+					if robot_ahead:
+						move.linear.x = 0.1
+					move.angular.z = 0.0
+					move.linear.y = 0.0
+					pub = rospy.Publisher("/robot2/cmd_vel", Twist, queue_size=2)
+					pub.publish(move)
+					print("target distance: {}".format(target_distance)) 
+				if target_distance <= 0.1:
+					move = Twist()
+					move.linear.x = 0.0
+					move.angular.z = 0.0
+					move.linear.y = 0.0
+					pub = rospy.Publisher("/robot2/cmd_vel", Twist, queue_size=2)
+					pub.publish(move)
+					print("reached target") 
+	if orders == "3":  # flyingV
+		Ready = False
+		if assigned:
+			if position1:
+				angle = atan2((MyY-2), (MyX-2)) - np.pi
+			if position2:
+				angle = atan2((MyY-1), (MyX-1)) - np.pi
+			if position3:
+				angle = atan2(MyY, (MyX-2)) - np.pi
+			if angle < -3.1415:
+				angle += (2*np.pi)
+			if (angle-0.3) < MyHeading < (angle + 0.3):
+				Ready = True
+				move = Twist()
+				move.linear.x = 0.0
+				move.angular.z = 0.0
+				move.linear.y = 0.0
+				pub = rospy.Publisher("/robot2/cmd_vel", Twist, queue_size=2)
+				pub.publish(move)
+			if not Ready:
+				target_distance = math.sqrt(((MyY-TargetY)**2)+((MyX-TargetX)**2))
+				if target_distance > 0.1:
+					if 0 <= MyHeading < (np.pi/2):  # get the rover to turn right in some cases, depending on its quadrant
+						if (np.pi/-2) <= angle <= 0:
+							right = True
+					if (np.pi/-2) <= MyHeading < 0:
+						if (np.pi*-1) <= angle <= (np.pi/-2):
+							right = True
+					if (np.pi/-1) <= MyHeading < (np.pi/-2):
+						if (np.pi/2) <= angle <= np.pi:
+							right = True
+					if (np.pi/2) <= MyHeading < np.pi:
+						if 0 <= angle <= (np.pi/2):
+							right = True
+					move = Twist()
+					move.linear.x = 0.0
+					move.angular.z = 1.0
+					move.linear.y = 0.0
+					if right:
+						move.angular.z = -1.0
+					pub = rospy.Publisher("/robot2/cmd_vel", Twist, queue_size=2)
+					pub.publish(move)
+					print("must face target")
+				if target_distance <= 0.1:
+					move = Twist()
+					move.linear.x = 0.0
+					move.angular.z = 0.0
+					move.linear.y = 0.0
+					pub = rospy.Publisher("/robot2/cmd_vel", Twist, queue_size=2)
+					pub.publish(move)
+					print("reached target")
+			if Ready:
+				right = False
+				target_distance = math.sqrt(((MyY-TargetY)**2)+((MyX-TargetX)**2))
+				if target_distance > 0.1:
+					move = Twist()
+					move.linear.x = 0.5
+					if robot_ahead:
+						move.linear.x = 0.1
+					move.angular.z = 0.0
+					move.linear.y = 0.0
+					pub = rospy.Publisher("/robot2/cmd_vel", Twist, queue_size=2)
+					pub.publish(move)
+					print("target distance: {}".format(target_distance)) 
+				if target_distance <= 0.1:
+					move = Twist()
+					move.linear.x = 0.0
+					move.angular.z = 0.0
+					move.linear.y = 0.0
+					pub = rospy.Publisher("/robot2/cmd_vel", Twist, queue_size=2)
+					pub.publish(move)
+					print("reached target") 
+# placeholder for orders 3 and orders 4
+	if orders == "5":
+		Ready = False
+		if assigned:
+			if position1:
+				angle = atan2(MyY-1.5, MyX-2) - np.pi
+			if position2:
+				angle = atan2(MyY-1.2, (MyX-1.25)) - np.pi
+			if position3:
+				angle = atan2(MyY-1.7, (MyX-1.25)) - np.pi
+			if angle < -3.1415:
+				angle += (2*np.pi)
+			if (angle-0.3) < MyHeading < (angle + 0.3):
+				Ready = True
+				move = Twist()
+				move.linear.x = 0.0
+				move.angular.z = 0.0
+				move.linear.y = 0.0
+				pub = rospy.Publisher("/robot2/cmd_vel", Twist, queue_size=2)
+				pub.publish(move)
+			if not Ready:
+				target_distance = math.sqrt(((MyY-TargetY)**2)+((MyX-TargetX)**2))
+				if target_distance > 0.1:
+					if 0 <= MyHeading < (np.pi/2):  # get the rover to turn right in some cases, depending on its quadrant
+						if (np.pi/-2) <= angle <= 0:
+							right = True
+					if (np.pi/-2) <= MyHeading < 0:
+						if (np.pi*-1) <= angle <= (np.pi/-2):
+							right = True
+					if (np.pi/-1) <= MyHeading < (np.pi/-2):
+						if (np.pi/2) <= angle <= np.pi:
+							right = True
+					if (np.pi/2) <= MyHeading < np.pi:
+						if 0 <= angle <= (np.pi/2):
+							right = True
+					move = Twist()
+					move.linear.x = 0.0
+					move.angular.z = 1.0
+					move.linear.y = 0.0
+					if right:
+						move.angular.z = -1.0
+					pub = rospy.Publisher("/robot2/cmd_vel", Twist, queue_size=2)
+					pub.publish(move)
+					print("must face target")
+				if target_distance <= 0.1:
+					move = Twist()
+					move.linear.x = 0.0
+					move.angular.z = 0.0
+					move.linear.y = 0.0
+					pub = rospy.Publisher("/robot2/cmd_vel", Twist, queue_size=2)
+					pub.publish(move)
+					print("reached target")
+			if Ready:
+				right = False
+				target_distance = math.sqrt(((MyY-TargetY)**2)+((MyX-TargetX)**2))
+				if target_distance > 0.1:
+					move = Twist()
+					move.linear.x = 0.5
+					if robot_ahead:
+						move.linear.x = 0.1
+					move.angular.z = 0.0
+					move.linear.y = 0.0
+					pub = rospy.Publisher("/robot2/cmd_vel", Twist, queue_size=2)
+					pub.publish(move)
+					print("target distance: {}".format(target_distance)) 
+				if target_distance <= 0.1:
+					move = Twist()
+					move.linear.x = 0.0
+					move.angular.z = 0.0
+					move.linear.y = 0.0
+					pub = rospy.Publisher("/robot2/cmd_vel", Twist, queue_size=2)
+					pub.publish(move)
+					print("reached target")
+
+
+def signal_handler(signal, frame):
+	move = Twist()
+	move.linear.x = 0.0
+	move.angular.z = 0.0
+	move.linear.y = 0.0
+	pub = rospy.Publisher("/robot2/cmd_vel", Twist, queue_size=2)
+	pub.publish(move)
+	print("exiting program")
+	sys.exit(0)
+
+
+def listener():
+	rospy.init_node('listener')
+	rospy.Subscriber("/chatter", String, update_orders)
+	rospy.Subscriber("/robot1/chatter", String, check_assignment)
+	rospy.Subscriber("/robot2/scan", LaserScan, check_laser)
+	rospy.Subscriber("/robot2/odom", Odometry, get_rotation_pos)
+	rospy.Subscriber("/robot1/odom", Odometry, check_robot1)
+	rospy.Subscriber("/robot3/odom", Odometry, check_robot3)
+	rospy.Subscriber("/robot2/odom", Odometry, get_rotation_pos)
+	rospy.Subscriber("/robot2/odom", Odometry, go)
+	rospy.spin()
+
+
+if __name__ == '__main__':
+	listener()
