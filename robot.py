060fd7ec0e0a629171d0459f3c595399350a413c
JustinPMaio <100385502+JustinPMaio@users.noreply.github.com>
2022-02-24 17:30:45 -0500

    Add files via upload

---
+++ robot.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+class Robot:
+	def __init__(self, x, y, heading, spot):
+		self.x = float(0)
+		self.y = float(0)
+		self.heading = float(0)
+		self.spot = float(0)
+
+class Spot:
+	def __init__(self, x, y, number):
+		self.x = float(0)
+		self.y = float(0)
+		self.occupied = bool(False)
+		self.number = float(0)
+
